17763919d7271ae5aaf668a3c1d0cd40360048e2
AndreNeves30 <andre.neves30@etec.sp.gov.br>
2022-10-27 21:58:25 -0300

    commit 4

--- app/src/main/java/br/com/etecia/foodfrast/MainActivity.java
+++ app/src/main/java/br/com/etecia/foodfrast/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,16 +20,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu,menu);
+        inflater.inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.mFav:
-
-
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no fav",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mBus:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei nessa coisa ai",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mConfif:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei nos confif",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mHist:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no lugar proibido",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mAbout:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no negocio",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mShare:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no chare",
+                        Toast.LENGTH_SHORT).show();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
